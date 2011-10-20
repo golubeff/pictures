bf4d1a56f1fe4558543440a7b1fca025b51b0d04
Paul Golubev <pavel@golubeff.ru>
2011-10-20 14:33:33 +0800

    image sizes

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -26,7 +26,7 @@ class Picture < ActiveRecord::Base
 
   protected
   def download_image
-    if self.changes.keys.include?(:source_url)
+    if self.changes.keys.include?(:source_url) || !File.exists?(path(:thumb)) || !File.exists?(path(:full))
       `wget #{self.source_url} -O /tmp/picture_#{self.id}`
       FileUtils.mkpath(directory)
       `convert /tmp/picture_#{self.id} -resize 80x80 -quality 61 #{path(:thumb)}`
